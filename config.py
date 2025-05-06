834be99ccc4b4eea80fb0d0f9a1aec69a43bc86c
natalyarockets <83515545+natalyarockets@users.noreply.github.com>
2025-05-06 11:16:26 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+#PDF_PATH = "/Users/n/Cirrus-AMM-Generator/work/SR22G5_AMM.pdf"
+PDF_PATH = "/Users/n/Library/CloudStorage/GoogleDrive-natalya@bettersquawk.com/Shared drives/Engineering/Manuals and documentation/SR22/SR22 vector store approved and in use/SR22 G5 POH - Perspective.pdf"
+EMBEDDING_MODEL = "text-embedding-3-small"  # Or text-embedding-3-large
+AIRCRAFT_MODEL = "SR22G5"      # e.g., "Boeing 737"
+
+#Need to add variable for document_type (POH, AMM, etc.)
